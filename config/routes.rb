c210e476703b37aa2dd1f055870ed877caf359a6
hiroji kimura <hirojikmr@gmail.com>
2016-02-18 23:02:55 +0900

    index画面更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  match ":controller(/:action(/:id))", :via=> :get 
+  get 'tasks/start' => 'tasks#start'
   resources :tasks
+  #get ':controller(/:action(/:id(.:format)))'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
