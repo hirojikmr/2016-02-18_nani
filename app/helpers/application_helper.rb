a1cd315f6e188418c80fa394310be58c6351e45c
hiroji kimura <hirojikmr@gmail.com>
2016-02-24 19:54:36 +0900

    hover.css追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  
+  def pick_eff
+
+    %w(
+      hvr-grow
+      hvr-shrink
+      hvr-pulse
+      hvr-pulse-grow
+      hvr-pulse-shrink
+      hvr-push
+      hvr-pop
+      hvr-bounce-in
+      hvr-bounce-out
+      hvr-rotate
+      hvr-grow-rotate
+      hvr-float
+      hvr-sink
+      hvr-bob
+      hvr-hang
+      hvr-skew
+      hvr-skew-forward
+      hvr-skew-backward
+      hvr-wobble-horizontal
+      hvr-wobble-vertical
+      hvr-wobble-to-bottom-right
+      hvr-wobble-to-top-right
+      hvr-wobble-top
+      hvr-wobble-bottom
+      hvr-wobble-skew
+      hvr-buzz
+      hvr-buzz-out
+    ).sample
+
+  end
 end
